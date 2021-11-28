eb34a9ce51c0175e91f30b263097529432f346d4
Martin Stefany <martin@stefany.eu>
2021-11-28 19:33:44 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,43 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
 #include <HardwareSerial.h>
+#include <math.h>
 #include <ModbusIP_ESP8266.h>
 #include <ModbusRTU.h>
 #include <Syslog.h>
 #include <WiFi.h>
 
+#include <secret.h>
+
 // https://support.innon.com/PowerMeters/SDM630-MOD-MID/Manual/SDM630-Modbus_Protocol.pdf
 #define EASTRON_SLAVE_ID 2
-#define EASTRON_RX_TX_PIN 21
-#define EASTRON_RX_PIN 22
-#define EASTRON_TX_PIN 23
+#define EASTRON_RX_TX_PIN 5
+#define EASTRON_RX_PIN 18
+#define EASTRON_TX_PIN 19
 #define INFINISOLAR_SLAVE_ID 2
-#define INFINISOLAR_RX_TX_PIN 17
-#define INFINISOLAR_RX_PIN 18
-#define INFINISOLAR_TX_PIN 19
+#define INFINISOLAR_RX_TX_PIN 21
+#define INFINISOLAR_RX_PIN 22
+#define INFINISOLAR_TX_PIN 23
 #define EASTRON_NUM_REGS 382
 
-#define WIFI_SSID "smarthome2_IoT"
-#define WIFI_PASSWORD "strongpassword"
+#define PWM1_GPIO 33
+#define PWM1_CH 0
+#define PWM1_RES 10
+#define PWM1_FREQ 10
+
 #define WIFI_TIMEOUT_MS 10000
 
 #define SYSLOG_SERVER "192.168.2.3"
 #define SYSLOG_PORT 514
-#define DEVICE_HOSTNAME "ESP32"
-#define APP_NAME "EastronMITM"
+#define DEVICE_HOSTNAME "sdm-replicator-7c9ebd4598b8"
+#define APP_NAME "sdm-replicator"
 
-IPAddress esp_ip(192, 168, 2, 49);
+#define MIN_PWM_POWER 200 // do not send less than 200 W
+#define MAX_PWM_POWER 1775 // this should be measured before
+#define MIN_PWM_DIFF 100 // do not act on small changes
+
+IPAddress esp_ip(192, 168, 2, 15);
 IPAddress esp_mask(255, 255, 255, 0);
 IPAddress esp_gw(192, 168, 2, 1);
 IPAddress esp_dns1(192, 168, 2, 1);
@@ -41,8 +53,32 @@ WiFiUDP udpClient;
 
 Syslog syslog(udpClient, SYSLOG_SERVER, SYSLOG_PORT, DEVICE_HOSTNAME, APP_NAME, LOG_KERN);
 
-BaseType_t xWiFiReturned;
-TaskHandle_t xWifiTaskHandle = NULL;
+TaskHandle_t infiniTaskHandle = NULL;
+TaskHandle_t espTaskHandle = NULL;
+TaskHandle_t otaTaskHandle = NULL;
+TaskHandle_t pwmTaskHandle = NULL;
+
+int16_t outPower = 0;
+int16_t outPowerLast = 0;
+uint16_t dutyCycle = 0;
+
+union 
+{
+  uint32_t x;
+  float f;
+} power1;
+
+union 
+{
+  uint32_t x;
+  float f;
+} power2;
+
+union 
+{
+  uint32_t x;
+  float f;
+} power3;
 
 bool cbSomebodyConnected(IPAddress ip) {
   Serial.print("New TCP client connected: ");
@@ -51,8 +87,31 @@ bool cbSomebodyConnected(IPAddress ip) {
   int n = ip.toString().length();
   char char_array[n + 1];
   strcpy(char_array, ip.toString().c_str());
-  syslog.logf(LOG_INFO, "New TCP client connected: %s", char_array);
+  if (WiFi.status() == WL_CONNECTED) {
+    syslog.logf(LOG_INFO, "New TCP client connected: %s", char_array);
+  }
   return true;
+}
+
+int calculateDutyCycle(int desiredPower) {
+  double percentage = desiredPower / MAX_PWM_POWER;
+  double fullArea = 2;
+  double desiredArea = fullArea * percentage;
+
+  double pwmResolution = pow(2, PWM1_RES);
+
+  double width = PI / pwmResolution; // rectangular width for integral approximation
+
+  double accumulated = 0;
+  for (int i = 1; i < pwmResolution; i++) {
+    accumulated += width * sin(width);
+
+    if (int(round(accumulated)) >= desiredArea) {
+      return i - 1;
+    }
+  }
+
+  return 0; // should not ever reach
 }
 
 uint16_t cbRead(TRegister* reg, uint16_t val) {
@@ -62,6 +121,7 @@ uint16_t cbRead(TRegister* reg, uint16_t val) {
 }
 
 void keepWifiTask(void *pvParameters) {
+  Serial.println("Started keepWifiTask");
   for(;;) {
     if (WiFi.status() == WL_CONNECTED) {
       vTaskDelay(10000 / portTICK_PERIOD_MS);
@@ -94,17 +154,35 @@ void keepWifiTask(void *pvParameters) {
     strcpy(char_array, WiFi.localIP().toString().c_str());
     syslog.logf(LOG_INFO, "WiFi connected. IP address: %s", char_array);
 
-    if (eTaskGetState(xWifiTaskHandle) == eSuspended) {
-      vTaskResume(xWifiTaskHandle);
+    if (espTaskHandle) {
+      xTaskNotifyGive(espTaskHandle);
     }
-  }
+
+    if (otaTaskHandle) {
+      xTaskNotifyGive(otaTaskHandle);
+    }
+
+    if (pwmTaskHandle) {
+      xTaskNotifyGive(pwmTaskHandle);
+    }
+  } 
 }
 
 void eastronTask(void *pvParameters) {
+  Serial.println("Started eastronTask");
+
   // Modbus RTU to power meter
   EastronSerial.begin(19200, SERIAL_8N1, EASTRON_RX_PIN, EASTRON_TX_PIN);
   EastronModbus.begin(&EastronSerial, EASTRON_RX_TX_PIN);
   EastronModbus.master();
+
+  if (infiniTaskHandle) {
+    xTaskNotifyGive(infiniTaskHandle);
+  }
+
+  if (pwmTaskHandle) {
+    xTaskNotifyGive(pwmTaskHandle);
+  }
 
   for(;;) {
     if (!EastronModbus.slave()) {
@@ -138,17 +216,26 @@ void eastronTask(void *pvParameters) {
         vTaskDelay(10 / portTICK_PERIOD_MS);
       }
     }
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
 void infiniTask(void *pvParameters) {
+  Serial.println("Started infiniTask");
+
+  xTaskNotifyWait(0x00,      /* Don't clear any notification bits on entry. */
+                  ULONG_MAX, /* Reset the notification value to 0 on exit. */
+                  NULL,
+                  portMAX_DELAY );  /* Block indefinitely. */
+
+  Serial.println("Unblocked infiniTask");
+
   // Modbus RTU to inverter
   InfiniSolarSerial.begin(19200, SERIAL_8N1, INFINISOLAR_RX_PIN, INFINISOLAR_TX_PIN);
   InfinisolarModbus.begin(&InfiniSolarSerial, INFINISOLAR_RX_TX_PIN);
   InfinisolarModbus.slave(INFINISOLAR_SLAVE_ID);
   InfinisolarModbus.addIreg(0, 0, EASTRON_NUM_REGS);
-  InfinisolarModbus.onGetIreg(0, cbRead, EASTRON_NUM_REGS);
+  //InfinisolarModbus.onGetIreg(0, cbRead, EASTRON_NUM_REGS);
 
   for(;;) {
     InfinisolarModbus.task();
@@ -156,11 +243,21 @@ void infiniTask(void *pvParameters) {
   }
 }
 
-void espTask(void *pvParameters) {
+void mbServerTask(void *pvParameters) {
+  Serial.println("Started mbServerTask");
+
+  xTaskNotifyWait(0x00,      /* Don't clear any notification bits on entry. */
+                  ULONG_MAX, /* Reset the notification value to 0 on exit. */
+                  NULL,
+                  portMAX_DELAY );  /* Block indefinitely. */
+
+  Serial.println("Unblocked mbServerTask");
+
   // Modbus IP for HA
   EspModbus.onConnect(cbSomebodyConnected);
   EspModbus.server();
   EspModbus.addIreg(0, 0, EASTRON_NUM_REGS);
+  EspModbus.addIreg(10000, 0, 10);
 
   for(;;) {
     EspModbus.task();
@@ -168,10 +265,218 @@ void espTask(void *pvParameters) {
   }
 }
 
+void otaTask(void *pvParameters) {
+  Serial.println("Started otaTask");
+
+  xTaskNotifyWait(0x00,      /* Don't clear any notification bits on entry. */
+                  ULONG_MAX, /* Reset the notification value to 0 on exit. */
+                  NULL,
+                  portMAX_DELAY );  /* Block indefinitely. */
+
+  Serial.println("Unblocked otaTask");
+
+  // ArduinoOTA.setPort(3232); 
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("\nAuth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("\nBegin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("\nConnect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("\nReceive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("\nEnd Failed");
+  });
+
+  Serial.println("OTA Initialized");
+
+  ArduinoOTA.begin();
+  for (;;) {
+    ArduinoOTA.handle();
+    delay(3500);
+  }
+}
+
+void pwmTask(void *pvParameters) {
+  Serial.println("Started pwmTask");
+
+  xTaskNotifyWait(0x00,      /* Don't clear any notification bits on entry. */
+                  ULONG_MAX, /* Reset the notification value to 0 on exit. */
+                  NULL,
+                  portMAX_DELAY );  /* Block indefinitely. */
+
+  Serial.println("Unblocked pwmTask once");
+
+  xTaskNotifyWait(0x00,      /* Don't clear any notification bits on entry. */
+                  ULONG_MAX, /* Reset the notification value to 0 on exit. */
+                  NULL,
+                  portMAX_DELAY );  /* Block indefinitely. */
+
+
+  Serial.println("Unblocked pwmTask twice");
+
+  ledcAttachPin(PWM1_GPIO, PWM1_CH);
+  ledcSetup(PWM1_CH, PWM1_FREQ, PWM1_RES);
+  ledcWrite(PWM1_CH, 0);
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  for(;;) {
+    power1.x = (((unsigned long)EastronModbus.Ireg(12) << 16) | EastronModbus.Ireg(13));
+    float p1 = power1.f;
+    Serial.print("Current L1 power: ");
+    Serial.println(p1);
+    if (WiFi.status() == WL_CONNECTED) {
+      syslog.logf(LOG_INFO, "Current L1 power: %f", p1);
+    }
+
+    power2.x = (((unsigned long)EastronModbus.Ireg(14) << 16) | EastronModbus.Ireg(15));
+    float p2 = power2.f;
+    Serial.print("Current L2 power: ");
+    Serial.println(p2);
+    if (WiFi.status() == WL_CONNECTED) {
+      syslog.logf(LOG_INFO, "Current L2 power: %f", p2);
+    }
+
+    power3.x = (((unsigned long)EastronModbus.Ireg(16) << 16) | EastronModbus.Ireg(17));
+    float p3 = power3.f;
+    Serial.print("Current L3 power: ");
+    Serial.println(p3);
+    if (WiFi.status() == WL_CONNECTED) {
+      syslog.logf(LOG_INFO, "Current L3 power: %f", p3);
+    }
+
+    // let's calculate how much we're exporting to grid or importing
+    float_t gridOverflow = p1 + p2 + p3;
+    EastronModbus.Ireg(10000, int(round(gridOverflow)));
+
+    float gridImport = 0;
+    float gridExport = 0;
+    if (gridOverflow >= 0) {
+      gridImport = round(gridOverflow);
+    } else {
+      gridExport = round(gridOverflow * -1);
+    }
+
+    Serial.print("Current gridImport: ");
+    Serial.println(gridImport);
+    if (WiFi.status() == WL_CONNECTED) {
+      syslog.logf(LOG_INFO, "Current gridImport: %f", gridImport);
+    }
+    EastronModbus.Ireg(10004, int(gridImport));
+
+    Serial.print("Current gridExport: ");
+    Serial.println(gridExport);
+    if (WiFi.status() == WL_CONNECTED) {
+      syslog.logf(LOG_INFO, "Current gridExport: %f", gridExport);
+    }
+    EastronModbus.Ireg(10005, int(gridExport));
+
+    // if we're sending something, redirect it to heating body
+    if (gridImport > 0) {
+      outPower -= int(gridImport);
+    } else if (gridExport > 0) {
+      outPower += int(gridExport);
+    }
+    if (outPower > MAX_PWM_POWER) {
+      outPower = MAX_PWM_POWER;
+    }
+    if (outPower < 0) {
+      outPower = 0;
+    }
+    Serial.print("Setting outPower to: ");
+    Serial.println(outPower);
+    if (WiFi.status() == WL_CONNECTED) {
+      syslog.logf(LOG_INFO, "Setting outPower to: %d", outPower);
+    }
+    EastronModbus.Ireg(10001, outPower);
+
+    if (outPower < MIN_PWM_POWER) {
+      dutyCycle = 0;
+      outPowerLast = 0;
+    } else if (abs(outPower - outPowerLast) > MIN_PWM_DIFF) {
+      dutyCycle = calculateDutyCycle(outPower);
+      outPowerLast = outPower;
+    }
+
+    Serial.print("Setting dutyCycle to: ");
+    Serial.println(dutyCycle);
+    if (WiFi.status() == WL_CONNECTED) {
+      syslog.logf(LOG_INFO, "Setting dutyCycle to: %d", dutyCycle);
+    }
+    EastronModbus.Ireg(10002, outPower);
+
+    ledcWrite(PWM1_CH, dutyCycle);
+
+    vTaskDelay(15000 / portTICK_PERIOD_MS);
+  }
+}
+
 void setup() {
   // serial console
   Serial.begin(115200);
 
+  xTaskCreate(
+    infiniTask,
+    "InfiniSolar",
+    2000,
+    NULL,
+    1,
+    &infiniTaskHandle
+  );
+
+  xTaskCreate(
+    otaTask,
+    "OTA",
+    10000,
+    NULL,
+    1,
+    &otaTaskHandle
+  );
+
+  xTaskCreate(
+    mbServerTask,
+    "ModbusTCP",
+    5000,
+    NULL,
+    1,
+    &espTaskHandle
+  );
+
+  xTaskCreate(
+    pwmTask,
+    "PWM",
+    10000,
+    NULL,
+    1,
+    &pwmTaskHandle
+  );
+
+  // must be one but last
   xTaskCreatePinnedToCore(
     keepWifiTask,
     "WifiTask",
@@ -182,15 +487,7 @@ void setup() {
     CONFIG_ARDUINO_RUNNING_CORE
   );
 
-  xTaskCreate(
-    infiniTask,
-    "InfiniSolar",
-    2000,
-    NULL,
-    1,
-    NULL
-  );
-
+  // must be last one
   xTaskCreatePinnedToCore(
     eastronTask,
     "Eastron",
@@ -200,19 +497,6 @@ void setup() {
     NULL,
     CONFIG_ARDUINO_RUNNING_CORE
   );
-
-  xWiFiReturned = xTaskCreate(
-    espTask,
-    "ModbusTCP",
-    5000,
-    NULL,
-    1,
-    &xWifiTaskHandle
-  );
-
-  if (xWiFiReturned == pdPASS) {
-    vTaskSuspend(xWifiTaskHandle);
-  }
 }
 
 void loop() {}
